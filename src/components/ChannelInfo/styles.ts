9d6bf9edff9c6145bbf86c9537aa4b086a3a2788
Gabriel Carmo <perien.op@gmail.com>
2021-02-13 16:35:37 -0300

    Updates

--- src/components/ChannelInfo/styles.ts
+++ src/components/ChannelInfo/styles.ts
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 import { Hashtag } from 'styled-icons/heroicons-outline';
 
 export const Container = styled.div`
-  grid-area: 'CI';
+  grid-area: CI;
 
   display: flex;
   align-items: center;
 
   padding: 0 17px;
   background-color: var(--primary);
-  
+
   box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
   z-index: 2;
 `;
@@ -21,7 +21,7 @@ export const HashtagIcon = styled(Hashtag)`
   color: var(--symbol);
 `;
 
-export const Title = styled.div`
+export const Title = styled.h1`
   margin-left: 9px;
 
   font-size: 16px;
@@ -36,6 +36,7 @@ export const Separator = styled.div`
 
   background-color: var(--white);
   opacity: 0.2;
+
   margin: 0 13px;
 `;
 
@@ -43,4 +44,3 @@ export const Description = styled.span`
   font-size: 15px;
   color: var(--gray);
 `;
-
